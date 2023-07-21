aeac03e0320e9dc804e7288a1f9123e10d6d2ab7
Vitor-HenriqueAS <vitorhenrique.a12@gmail.com>
2023-07-21 11:19:30 -0300

    update css and create component Raridade

---
+++ web/src/components/RaridadeJuros/Raridade.vue
@@ -0,0 +1,25 @@
+<script setup>
+  defineProps({   
+  nivel: String,
+  tier1: Int16Array,
+  tier2: Int16Array,
+  tier3: Int16Array,
+  tier4: Int16Array,
+  tier5: Int16Array
+  })
+</script>
+
+<template>
+  <div>
+    Olá Mundo
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'RaridadeNivel'
+  }
+</script>
+
+<style>
+</style>
