a11c0c8902d134303685add19fa6b67daa667c4f
Vitor-HenriqueAS <vitorhenrique.a12@gmail.com>
2023-08-15 10:09:52 -0300

    create template

--- web/src/views/ComposicoesView.vue
+++ web/src/views/ComposicoesView.vue
@@ -1,13 +1,115 @@
 <template>
-  <main>
-    <h1>Composições</h1>
-    <ul>
-      <li>Criação de Comps</li>
-      <li>Ícones das comps</li>
-      <li>Integrantes</li>
-      <li>Nome da comp personalizável</li>
-    </ul>
-  </main>
+  <section>
+
+    <header>
+      <h1>Composições</h1>
+      <h3>Faca sua composição vitoriosa</h3>
+    </header>
+
+    <div class="slots">
+      <div class="linha">
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+      </div>
+      <div class="linha">
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+      </div>
+      <div class="linha">
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+      </div>
+      <div class="linha">
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+      </div>
+
+      <button>Baixar</button>
+      <button>Compartilhar</button>
+    </div>
+
+    <div class="campeoes">
+      <div class="tier">
+        <div class="tier__cabecalho">
+          <h2>tier I</h2>
+          <div class="custo">
+            1 moeda
+          </div>
+          <div class="tier__campeao">
+            varias divs com img dos campeoes
+          </div>
+        </div>
+      </div>
+      
+      <div class="tier">
+        <div class="tier__cabecalho">
+          <h2>tier I</h2>
+          <div class="custo">
+            1 moeda
+          </div>
+          <div class="tier__campeao">
+            varias divs com img dos campeoes
+          </div>
+        </div>
+      </div>
+
+      <div class="tier">
+        <div class="tier__cabecalho">
+          <h2>tier I</h2>
+          <div class="custo">
+            1 moeda
+          </div>
+          <div class="tier__campeao">
+            varias divs com img dos campeoes
+          </div>
+        </div>
+      </div>
+
+      <div class="tier">
+        <div class="tier__cabecalho">
+          <h2>tier I</h2>
+          <div class="custo">
+            1 moeda
+          </div>
+          <div class="tier__campeao">
+            varias divs com img dos campeoes
+          </div>
+        </div>
+      </div>
+
+      <div class="tier">
+        <div class="tier__cabecalho">
+          <h2>tier I</h2>
+          <div class="custo">
+            1 moeda
+          </div>
+          <div class="tier__campeao">
+            varias divs com img dos campeoes
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
